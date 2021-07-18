16b83bf1597b82e7b6fe162eb3c6a1eb8d0694bd
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2021-07-18 14:08:28 +0300

    PTL #5686 admin/tool/objectfs (patch) WIP add support for component, filearea, mimetype filters

--- admin/tool/objectfs/classes/local/object_manipulator/candidates/pusher_candidates.php
+++ admin/tool/objectfs/classes/local/object_manipulator/candidates/pusher_candidates.php
@@ -36,13 +36,59 @@ class pusher_candidates extends manipulator_candidates_base {
      * @return string
      */
     public function get_candidates_sql() {
-        return 'SELECT contenthash,
-                       filesize
-                  FROM {tool_objectfs_objects}
-                 WHERE filesize > :threshold
-                   AND filesize < :maximum_file_size
-                   AND timeduplicated <= :maxcreatedtimestamp
-                   AND location = :object_location';
+        // Filter file component.
+        $filtercomponent = [];
+        if (!empty($this->config->filtercomponent)) {
+            foreach(explode(',', $this->config->filtercomponent) as $item){
+                if(!empty(trim($item))){
+                    $filtercomponent[] = trim($item);
+                }
+            }
+
+            $filtercomponent = array_unique($filtercomponent);
+        }
+
+        $strcomponent = !empty($filtercomponent) ? " AND f.component IN ('".implode("','", $filtercomponent)."') " : '';
+
+        // Filter file filearea.
+        $filterfilearea = [];
+        if (!empty($this->config->filterfilearea)) {
+            foreach(explode(',', $this->config->filterfilearea) as $item){
+                if(!empty(trim($item))){
+                    $filterfilearea[] = trim($item);
+                }
+            }
+
+            $filterfilearea = array_unique($filterfilearea);
+        }
+
+        $strfilearea = !empty($filterfilearea) ? " AND f.filearea IN ('".implode("','", $filterfilearea)."') " : '';
+
+        // Filter file mimetype.
+        $filtermimetype = [];
+        if (!empty($this->config->filtermimetype)) {
+            foreach(explode(',', $this->config->filtermimetype) as $item){
+                if(!empty(trim($item))){
+                    $filtermimetype[] = trim($item);
+                }
+            }
+
+            $filtermimetype = array_unique($filtermimetype);
+        }
+
+        $strmimetype = !empty($filtermimetype) ? " AND f.mimetype IN ('".implode("','", $filtermimetype)."') " : '';
+
+        return 'SELECT f.contenthash,
+                       f.filesize
+                  FROM {files} f
+                  JOIN {tool_objectfs_objects} o ON f.contenthash = o.contenthash
+                 WHERE f.filesize > :threshold
+                   AND f.filesize < :maximum_file_size
+                   '.$strcomponent.'
+                   '.$strfilearea.'
+                   '.$strmimetype.'
+                   AND o.timeduplicated <= :maxcreatedtimestamp
+                   AND f.location = :object_location';
     }
 
     /**
@@ -54,6 +100,9 @@ class pusher_candidates extends manipulator_candidates_base {
         return [
             'maxcreatedtimestamp' => time() - $this->config->minimumage,
             'threshold' => $this->config->sizethreshold,
+            'component' => $this->config->component,
+            'filearea' => $this->config->filearea,
+            'mimetype' => $this->config->mimetype,
             'maximum_file_size' => $filesystem->get_maximum_upload_filesize(),
             'object_location' => OBJECT_LOCATION_LOCAL,
         ];
