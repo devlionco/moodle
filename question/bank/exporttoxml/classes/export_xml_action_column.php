catalyst/moodle-MDL-70329

MDL-71639 qbank_exporttoxml: Add Export to xml plugin to core
This implementation will introduce a qbank plugin "exporttoxml"
which will implement the export to xml action in the question
bank view by replacing the core class. Having this plugin will
give users the flexibility of enabling or disabling this action.

question/bank/exporttoxml/classes/export_xml_action_column.php
@@ -17,36 +17,61 @@
 /**
  * Question bank column export the question in Moodle XML format.
  *
- * @package   core_question
+ * @package   qbank_exporttoxml
  * @copyright 2019 The Open University
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace core_question\bank;
+namespace qbank_exporttoxml;
+
 defined('MOODLE_INTERNAL') || die();
 
+use core_question\local\bank\menu_action_column_base;
 
 /**
  * Question bank column export the question in Moodle XML format.
  *
+ * @package   qbank_exporttoxml
  * @copyright 2019 The Open University
+ * @author    2021 Safat Shahin <safatshahin@catalyst-au.net>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @deprecated since Moodle 4.0
- * @see \qbank_exporttoxml\export_xml_action_column
  */
 class export_xml_action_column extends menu_action_column_base {
+
     /** @var string avoids repeated calls to get_string('duplicate'). */
     protected $strexportasxml;
 
-    public function init() {
+    /**
+     * A chance for subclasses to initialise themselves, for example to load lang strings,
+     * without having to override the constructor.
+     */
+    public function init(): void {
         parent::init();
         $this->strexportasxml = get_string('exportasxml', 'question');
     }
 
-    public function get_name() {
+    /**
+     * Get the internal name for this column. Used as a CSS class name,
+     * and to store information about the current sort. Must match PARAM_ALPHA.
+     *
+     * @return string column name.
+     */
+    public function get_name(): string {
         return 'exportasxmlaction';
     }
 
+    /**
+     * Get the information required to display this action either as a menu item or a separate action column.
+     *
+     * If this action cannot apply to this question (e.g. because the user does not have
+     * permission, then return [null, null, null].
+     *
+     * @param \stdClass $question the row from the $question table, augmented with extra information.
+     * @return array with three elements.
+     *      $url - the URL to perform the action.
+     *      $icon - the icon for this action. E.g. 't/delete'.
+     *      $label - text label to display in the UI (either in the menu, or as a tool-tip on the icon)
+     */
     protected function get_url_icon_and_label(\stdClass $question): array {
         if (!\question_bank::is_qtype_installed($question->qtype)) {
             // It sometimes happens that people end up with junk questions
@@ -59,7 +84,9 @@ class export_xml_action_column extends menu_action_column_base {
             return [null, null, null];
         }
 
-        return [question_get_export_single_question_url($question),
+        return [exporttoxml_helper::question_get_export_single_question_url($question),
                 't/download', $this->strexportasxml];
     }
+
 }
+

question/bank/exporttoxml/exportone.php
@@ -1,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Script to download the export of a single question.
+ *
+ * @package    qbank_exporttoxml
+ * @copyright  2015 the Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../config.php');
+require_once($CFG->libdir . '/questionlib.php');
+require_once($CFG->dirroot . '/question/format/xml/format.php');
+
+// Get the parameters from the URL.
+$questionid = required_param('id', PARAM_INT);
+$cmid = optional_param('cmid', 0, PARAM_INT);
+$courseid = optional_param('courseid', 0, PARAM_INT);
+$urlparams = ['id' => $questionid, 'sesskey' => sesskey()];
+
+\core_question\local\bank\helper::check_qbank_status('qbank_exporttoxml');
+
+if ($cmid) {
+    $cm = get_coursemodule_from_id(null, $cmid);
+    require_login($cm->course, false, $cm);
+    $thiscontext = context_module::instance($cmid);
+    $urlparams['cmid'] = $cmid;
+} else if ($courseid) {
+    require_login($courseid, false);
+    $thiscontext = context_course::instance($courseid);
+    $urlparams['courseid'] = $courseid;
+} else {
+    throw new moodle_exception('missingcourseorcmid', 'question');
+}
+require_sesskey();
+
+// Load the necessary data.
+$contexts = new question_edit_contexts($thiscontext);
+$questiondata = question_bank::load_question_data($questionid);
+
+// Check permissions.
+question_require_capability_on($questiondata, 'view');
+
+// Initialise $PAGE. Nothing is output, so this does not really matter. Just avoids notices.
+$nexturl = new moodle_url('/question/type/stack/questiontestrun.php', $urlparams);
+$PAGE->set_url('/question/bank/exporttoxml/exportone.php', $urlparams);
+$PAGE->set_heading($COURSE->fullname);
+$PAGE->set_pagelayout('admin');
+
+// Set up the export format.
+$qformat = new qformat_xml();
+$filename = question_default_export_filename($COURSE, $questiondata) .
+        $qformat->export_file_extension();
+$qformat->setContexts($contexts->having_one_edit_tab_cap('export'));
+$qformat->setCourse($COURSE);
+$qformat->setQuestions([$questiondata]);
+$qformat->setCattofile(false);
+$qformat->setContexttofile(false);
+
+// Do the export.
+if (!$qformat->exportpreprocess()) {
+    send_file_not_found();
+}
+if (!$content = $qformat->exportprocess(true)) {
+    send_file_not_found();
+}
+send_file($content, $filename, 0, 0, true, true, $qformat->mime_type());
+

devlionco/moodle

question/bank/exporttoxml/classes/export_xml_action_column.php
@@ -14,35 +14,29 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Question bank column export the question in Moodle XML format.
- *
- * @package   core_question
- * @copyright 2019 The Open University
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+namespace qbank_exporttoxml;
 
-namespace core_question\bank;
-defined('MOODLE_INTERNAL') || die();
-
+use core_question\local\bank\menu_action_column_base;
 
 /**
  * Question bank column export the question in Moodle XML format.
  *
+ * @package   qbank_exporttoxml
  * @copyright 2019 The Open University
+ * @author    2021 Safat Shahin <safatshahin@catalyst-au.net>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @todo MDL-72004 delete the class and add it to lib/db/renameclasses.php pointing to the plugin
  */
 class export_xml_action_column extends menu_action_column_base {
+
     /** @var string avoids repeated calls to get_string('duplicate'). */
     protected $strexportasxml;
 
-    public function init() {
+    public function init(): void {
         parent::init();
         $this->strexportasxml = get_string('exportasxml', 'question');
     }
 
-    public function get_name() {
+    public function get_name(): string {
         return 'exportasxmlaction';
     }
 
@@ -58,7 +52,8 @@ class export_xml_action_column extends menu_action_column_base {
             return [null, null, null];
         }
 
-        return [\qbank_exporttoxml\helper::question_get_export_single_question_url($question),
+        return [helper::question_get_export_single_question_url($question),
                 't/download', $this->strexportasxml];
     }
+
 }

question/bank/exporttoxml/exportone.php
@@ -1,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Script to download the export of a single question.
+ *
+ * @package    qbank_exporttoxml
+ * @copyright  2015 the Open University
+ * @author     Safat Shahin <safatshahin@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../config.php');
+require_once($CFG->libdir . '/questionlib.php');
+require_once($CFG->dirroot . '/question/format/xml/format.php');
+
+// Get the parameters from the URL.
+$questionid = required_param('id', PARAM_INT);
+$cmid = optional_param('cmid', 0, PARAM_INT);
+$courseid = optional_param('courseid', 0, PARAM_INT);
+$urlparams = ['id' => $questionid, 'sesskey' => sesskey()];
+
+\core_question\local\bank\helper::require_plugin_enabled('qbank_exporttoxml');
+
+if ($cmid) {
+    $cm = get_coursemodule_from_id(null, $cmid);
+    require_login($cm->course, false, $cm);
+    $thiscontext = context_module::instance($cmid);
+    $urlparams['cmid'] = $cmid;
+} else if ($courseid) {
+    require_login($courseid, false);
+    $thiscontext = context_course::instance($courseid);
+    $urlparams['courseid'] = $courseid;
+} else {
+    throw new moodle_exception('missingcourseorcmid', 'question');
+}
+require_sesskey();
+
+// Load the necessary data.
+$contexts = new question_edit_contexts($thiscontext);
+$questiondata = question_bank::load_question_data($questionid);
+
+// Check permissions.
+question_require_capability_on($questiondata, 'view');
+
+// Initialise $PAGE. Nothing is output, so this does not really matter. Just avoids notices.
+$nexturl = new moodle_url('/question/type/stack/questiontestrun.php', $urlparams);
+$PAGE->set_url('/question/bank/exporttoxml/exportone.php', $urlparams);
+$PAGE->set_heading($COURSE->fullname);
+$PAGE->set_pagelayout('admin');
+
+// Set up the export format.
+$qformat = new qformat_xml();
+$filename = question_default_export_filename($COURSE, $questiondata) .
+        $qformat->export_file_extension();
+$qformat->setContexts($contexts->having_one_edit_tab_cap('export'));
+$qformat->setCourse($COURSE);
+$qformat->setQuestions([$questiondata]);
+$qformat->setCattofile(false);
+$qformat->setContexttofile(false);
+
+// Do the export.
+if (!$qformat->exportpreprocess()) {
+    send_file_not_found();
+}
+if (!$content = $qformat->exportprocess(true)) {
+    send_file_not_found();
+}
+send_file($content, $filename, 0, 0, true, true, $qformat->mime_type());