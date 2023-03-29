35e82f7c02403fd332a3bc74f0ca8c0e5baa7008
oleg <oleg@devlion.co>
2023-03-29 09:32:43 +0300

    qtype_calcsheet (add) Init latest version.

---
+++ question/type/calcsheet/lang/he/qtype_calcsheet.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_calcsheet', language 'he', branch 'MOODLE_35_STABLE'
+ *
+ * @package    qtype
+ * @subpackage calcsheet
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addmoreanswerblanks'] = 'הוספת {no} אפשרויות תשובה חדשות';
+$string['answer'] = 'תשובה: {$a}';
+$string['answermustbegiven'] = 'יש להזין תוכן לתשובה, כאשר קיים משוב או ציון.';
+$string['answerno'] = 'תשובה {$a}';
+$string['caseno'] = 'אין חשיבות לאותיות גדולות או קטנות';
+$string['casesensitive'] = 'יש חשיבות לגודל האותיות';
+$string['caseyes'] = 'יש לדייק בגודל הכתב, אותיות גדולות וקטנות';
+$string['correctansweris'] = 'התשובה הנכונה היא: {$a}';
+$string['correctanswers'] = 'תשובות נכונות';
+$string['filloutoneanswer'] = 'יש להזין לפחות תשובה אחת מלאה. תשובות ריקות לא ייחשבו. הסימן \'*\' יחשב כמחליף לכל תשובה אפשרית. התשובה הנכונה הראשונה שתתאים מהרשימה תבחר ותוצג לתלמיד כמשוב וציון.';
+$string['notenoughanswers'] = 'סוג שאלה זו דורש לפחות {$a} תשובות';
+$string['pleaseenterananswer'] = 'יש להזין תשובה.';
+$string['pluginname'] = 'גליון חישוב';
+$string['pluginname_help'] = 'In response to a question (that may include a image) the respondent types a word or short phrase. There may be several possible correct answers, each with a different grade. If the "Case sensitive" option is selected, then you can have different scores for "Word" or "word".';
+$string['pluginname_link'] = 'question/type/calcsheet';
+$string['pluginnameadding'] = 'הוספת שאלת גליון חישוב';
+$string['pluginnameediting'] = 'עריכת שאלת גיליון חישוב';
+$string['pluginnamesummary'] = 'A spreadsheet plugin that has the basic functionality like formulas, adding cols,rows, compares the answer and grade it';
+$string['prellenado'] = 'תבנית תשובה לתלמיד';
+$string['soloyes'] = 'לא, רק ערכים נדרשים';
+$string['solono'] = 'כן, נוסחאות וערכים נדרשים';
+$string['soloform'] = 'כן, רק נוסחאות נדרשות';
+$string['solovalor'] = 'האם נוסחאות נדרשות';
+$string['valor'] = 'משקל התשובה';
+
+
+//$string['pluginnamesummary'] = 'Allows a response of one or a few words that is graded by comparing against various model answers, which may contain wildcards.';
