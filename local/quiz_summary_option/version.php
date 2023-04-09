e1e071a8300306fece5e38e2957f6ddd98882dd3
oleg <oleg@devlion.co>
2023-04-09 15:12:16 +0300

    PTL #8622 local_quiz_summary_option (add) Changes from m39_petel (8113, 6970, 7862, 5592)

---
+++ local/quiz_summary_option/version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package   local_quiz_summary_option
+ * @author    Christina Roperto (christinatheeroperto@catalyst-au.net)
+ * @copyright Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2021060908;
+$plugin->release   = '0.1';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->requires  = 2020061500; // Moodle 3.9 release and upwards.
+$plugin->component = 'local_quiz_summary_option';
