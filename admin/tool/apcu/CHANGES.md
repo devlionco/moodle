92d5d16ec9e2a5efb3d18decc281cb822730a1b5
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-04-12 23:49:35 +0300

    admin/tool/apcu (new) manage APCu cache ver.2023010500

---
+++ admin/tool/apcu/CHANGES.md
@@ -0,0 +1,61 @@
+moodle-tool_apcu
+================
+
+Changes
+-------
+
+### v4.1-r1
+
+* 2023-01-21 - Prepare compatibility for Moodle 4.1.
+* 2022-11-28 - Updated Moodle Plugin CI to latest upstream recommendations
+
+### v4.0-r1
+
+* 2022-07-12 - Prepare compatibility for Moodle 4.0.
+
+### v3.11-r3
+
+* 2022-07-10 - Add Visual checks section to UPGRADE.md
+* 2022-07-10 - Add Capabilities section to README.md
+
+### v3.11-r2
+
+* 2022-06-26 - Make codechecker happy again
+* 2022-06-26 - Updated Moodle Plugin CI to latest upstream recommendations
+* 2022-06-26 - Add UPGRADE.md as internal upgrade documentation
+* 2022-06-26 - Update maintainers and copyrights in README.md.
+
+### v3.11-r1
+
+* 2021-12-08 - Prepare compatibility for Moodle 3.11.
+
+### v3.10-r2
+
+* 2021-09-29 - Replace the deprecated print_error() function with a Moodle exception
+* 2021-02-05 - Move Moodle Plugin CI from Travis CI to Github actions
+
+### v3.10-r1
+
+* 2021-01-09 - Prepare compatibility for Moodle 3.10.
+* 2021-01-06 - Change in Moodle release support:
+               For the time being, this plugin is maintained for the most recent LTS release of Moodle as well as the most recent major release of Moodle.
+               Bugfixes are backported to the LTS release. However, new features and improvements are not necessarily backported to the LTS release.
+* 2021-01-06 - Improvement: Declare which major stable version of Moodle this plugin supports (see MDL-59562 for details).
+
+### v3.9-r1
+
+* 2020-09-28 - Prepare compatibility for Moodle 3.9.
+
+### v3.8-r1
+
+* 2020-09-28 - Prepare compatibility for Moodle 3.8.
+
+### v3.7-r2
+
+* 2020-09-29 - Remove the APCu GUI file from the plugin codebase as this is a requirement for approval of the plugin on moodle.org/plugins.
+               A download and store mechanism was implemented and runs during installation and upgrade of this plugin. If this mechanism fails, the administrator is informed what he has to do to get the plugin up and running (again).
+* 2020-09-28 - Remove hardcoded admin directory in paths.
+
+### v3.7-r1
+
+* 2020-05-03 - Initial version
---
+++ admin/tool/apcu/locallib.php
@@ -0,0 +1,192 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Admin tool "APCu management" - Local library
+ *
+ * @package    tool_apcu
+ * @copyright  2020 Alexander Bias, Ulm University <alexander.bias@uni-ulm.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Helper function to prefix given CSS code.
+ *
+ * @author Spooky
+ * @copyright 2016 https://stackoverflow.com/a/35253188
+ *
+ * @param string $css
+ * @param string $prefix
+ *
+ * @return string
+ */
+function tool_apcu_get_prefixed_css($css, $prefix) {
+    $css = preg_replace('!/\*.*?\*/!s', '', $css);
+
+    $parts = explode('}', $css);
+    $mediaquerystarted = false;
+
+    foreach ($parts as &$part) {
+        $part = trim($part);
+        if (empty($part)) {
+            continue;
+        } else {
+            $partdetails = explode('{', $part);
+            if (substr_count($part, "{") == 2) {
+                $mediaquery = $partdetails[0]."{";
+                $partdetails[0] = $partdetails[1];
+                $mediaquerystarted = true;
+            }
+
+            $subparts = explode(',', $partdetails[0]);
+            foreach ($subparts as &$subpart) {
+                if (trim($subpart) === "@font-face") {
+                    continue;
+                } else {
+                    $subpart = $prefix . ' ' . trim($subpart);
+                }
+            }
+
+            if (substr_count($part, "{") == 2) {
+                $part = $mediaquery."\n".implode(', ', $subparts)."{".$partdetails[2];
+            } else if (empty($part[0]) && $mediaquerystarted) {
+                $mediaquerystarted = false;
+                $part = implode(', ', $subparts)."{".$partdetails[2]."}\n";
+            } else {
+                if (isset($partdetails[1])) {
+                    $part = implode(', ', $subparts)."{".$partdetails[1];
+                }
+            }
+
+            unset($partdetails, $mediaquery, $subparts);
+        }
+
+        unset($part);
+    }
+
+    return(preg_replace('/\s+/', ' ', implode("} ", $parts)));
+}
+
+/**
+ * Helper function to download and store APCu management GUI file.
+ *
+ * @return bool
+ */
+function tool_apcu_do_guidrop() {
+    global $CFG;
+
+    // Require Filelib for using cURL.
+    require_once($CFG->libdir.'/filelib.php');
+
+    // Get the target directory and path.
+    $targetdir = tool_apcu_get_guidrop_directory();
+    $targetpath = tool_apcu_get_guidrop_path();
+
+    // If we would be unable to store the APCu GUI file, return.
+    if (is_dir($targetdir) != true || is_writable($targetdir) != true) {
+        return false;
+    }
+
+    // Try to download the APCu GUI file.
+    $curl = new \curl;
+    $downloadtmp = make_request_directory();
+    $downloadto = $downloadtmp.'/apcu.php.inc';
+    $downloadoptions = ['filepath' => $downloadto, 'timeout' => 60, 'followlocation' => true, 'maxredirs' => 0];
+    $downloadsuccess = $curl->download_one(tool_apcu_get_guidrop_url(), null, $downloadoptions);
+
+    // If the download was not successful, return.
+    if ($downloadsuccess != true) {
+        return false;
+    }
+
+    // Try to move the APCu GUI file to the target directory.
+    $renamesuccess = rename($downloadto, $targetpath);
+
+    // If the rename command was not successful, return.
+    if ($renamesuccess != true) {
+        return false;
+    }
+
+    // If, in the end, the APCu GUI file isn't at the target path or does not contain the right content, return.
+    if (tool_apcu_verify_guidrop_file() != true) {
+        return false;
+    }
+
+    // If we have reached this point, the APCu GUI management file should be placed correctly.
+    return true;
+}
+
+/**
+ * Helper function to get the URL where to download the APCu management GUI file.
+ *
+ * @return string
+ */
+function tool_apcu_get_guidrop_url() {
+    // The source where the APCu GUI file is downloaded from.
+    return 'https://raw.githubusercontent.com/krakjoe/apcu/master/apc.php';
+}
+
+/**
+ * Helper function to get the directory where to store the APCu management GUI file.
+ *
+ * @return string
+ */
+function tool_apcu_get_guidrop_directory() {
+    global $CFG;
+
+    // The directory where the APCu GUI file should be stored.
+    return $CFG->dirroot.'/'.$CFG->admin.'/tool/apcu/lib/apcu-gui';
+}
+
+/**
+ * Helper function to get the directory and filename where to store the APCu management GUI file.
+ *
+ * @return string
+ */
+function tool_apcu_get_guidrop_path() {
+    global $CFG;
+
+    // The directory and filename where the APCu GUI file should be stored.
+    return $CFG->dirroot.'/'.$CFG->admin.'/tool/apcu/lib/apcu-gui/apcu.php.inc';
+}
+
+/**
+ * Helper function to verify if the APCu management GUI file exists and has the right content.
+ *
+ * @return string
+ */
+function tool_apcu_verify_guidrop_file() {
+    // Get the path where the file is expected.
+    $targetpath = tool_apcu_get_guidrop_path();
+
+    // If there isn't a readable file at the target path, return.
+    if (file_exists ($targetpath) != true || is_readable($targetpath) != true) {
+        return false;
+    }
+
+    // If the file does not contain characteristic code snippets, return.
+    $filestring = file_get_contents($targetpath);
+    if (strpos($filestring, 'Authors: Ralf Becker') === false ||
+            strpos($filestring, 'General Cache Information') === false ||
+            strpos($filestring, 'Cache Information') === false ||
+            strpos($filestring, 'Runtime Settings') === false ||
+            strpos($filestring, 'Host Status Diagrams') === false) {
+        return false;
+    }
+
+    // If we have reached this point, the APCu GUI management file should be fine.
+    return true;
+}
