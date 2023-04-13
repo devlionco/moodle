48fc65a7e83d28742878351f4221390d445de70c
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-04-14 02:15:11 +0300

    mod/mindmaap (new) ver.2019080815 copy from petel m39 (Devlion)

---
+++ mod/mindmaap/lang/he/mindmaap.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_mindmaap
+ * @category    string
+ * @copyright   2019 Devlion <info@devlion.co>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'מפות חשיבה';
+$string['pluginname'] = 'מפות חשיבה';
+$string['mindmaapname'] = 'שם';
+$string['mindmaapname_help'] = 'שם הפעילות';
+$string['mindmaapsettings'] = 'מפות חשיבה הגדרות';
+$string['mindmaapfieldset'] = 'תצוגה חזותית';
+$string['type'] = 'סוג';
+$string['pluginadministration'] = 'ניהול מפות חשיבה';
+
+$string['mindmaap_token'] = 'אסימון לשירות מפות חשיבה';
+$string['mindmaap_token_desc'] = 'לקבלת גישות נא לגלוש באתר <a href="https://mindmaap.com">MindMaap</a>';
+$string['mindmaap_url'] = 'קישור לשירות מפות חשיבה';
+$string['mindmaap_url_desc'] = 'לקבלת גישות נא לגלוש באתר<a href="https://mindmaap.com">MindMaap</a>';
+
+$string['openpopup'] = 'הצגת מפת חשיבה';
+$string['newwindow'] = 'הצגת מפת חשיבה';
+$string['directlink'] = 'הצגת מפת חשיבה';
+$string['modulenameplural'] = 'mindmaaps';
+
+$string['type_iframe'] = 'משובץ בעמוד הקורס';
+$string['type_window'] = 'חלון חדש';
+$string['type_popup'] = 'חלון צף';
+$string['type_link'] = 'קישור';
+
+$string['mindmaap:addinstance'] = 'להוסיף פעילות מפת חשיבה';
+$string['mindmaap:view'] = 'לצפות בפעילות מפת חשיבה';
