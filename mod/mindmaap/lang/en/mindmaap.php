7ec0c2370977d7b3a7ffd00c77503a6f2a945130
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-04-14 02:15:11 +0300

    mod/mindmaap (new) ver.2019080815 copy from petel m39 (Devlion)

---
+++ mod/mindmaap/lang/en/mindmaap.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_mindmaap
+ * @category    string
+ * @copyright   2019 Devlion <info@devlion.co>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Mind Map';
+$string['pluginname'] = 'Mind Map';
+$string['mindmaapname'] = 'Name';
+$string['mindmaapname_help'] = 'Activity name';
+$string['mindmaapsettings'] = 'Activity settings';
+$string['mindmaapfieldset'] = 'Appearance';
+$string['type'] = 'Mind Map Type';
+$string['pluginadministration'] = 'MindMaap Administration';
+
+$string['mindmaap_token'] = 'Mind Map Service Token';
+$string['mindmaap_token_desc'] = 'Get token on site <a href="https://mindmaap.com">MindMaap</a>';
+$string['mindmaap_url'] = 'Mind Map Service Url';
+$string['mindmaap_url_desc'] = 'Get url on site <a href="https://mindmaap.com">MindMaap</a>';
+
+$string['openpopup'] = 'Start Mind Map';
+$string['newwindow'] = 'Start Mind Map';
+$string['directlink'] = 'Start Mind Map';
+$string['modulenameplural'] = 'mindmaps';
+$string['nonewmodules'] = 'No new modules';
+
+$string['type_iframe'] = 'Iframe';
+$string['type_window'] = 'New Window';
+$string['type_popup'] = 'Pop Up';
+$string['type_link'] = 'Link';
+
+$string['curlerror'] = 'Error occured during server request: {$a}';
+$string['missingidandcmid'] = 'id or cmid missing';
+
+$string['privacy:metadata'] = 'The mod_mindmaap plugin does not store any personal data.';
+
+$string['mindmaap:addinstance'] = 'Mind Map add instance';
+$string['mindmaap:view'] = 'Mind Map view';
