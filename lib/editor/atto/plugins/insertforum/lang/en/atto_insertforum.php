1ae30f07532b6499f15a79e6ac83a60aa56cec91
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-04-15 21:38:41 +0300

    atto/insertforum (new) ver.2016062600 copy from petel m39

---
+++ lib/editor/atto/plugins/insertforum/lang/en/atto_insertforum.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_insertforum', language 'en'.
+ *
+ * @package    atto_insertforum
+ * @author     Nadav Kavalerchik <nadavkav@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['forum'] = 'Insert a Forum';
+$string['insertforum'] = 'Insert a Forum (filter)';
+$string['noforums'] = 'Sorry, no Forums found to insert.';
+$string['pluginname'] = 'Insert Forum';
+$string['select_desc'] = 'Select the Forum you want to insert below. Note, users may not be able to see the forum if any of its visiblity options and/or the users permissions prevent them from seeing it.';
+$string['title'] = 'Select a Forum';
+$string['selectgroup'] = 'Select a group';
+$string['groups'] = 'Group';
+$string['nogroups'] = 'No groups';
+$string['selectgrouping'] = 'Select a grouping';
+$string['groupings'] = 'Grouping';
+$string['nogroupings'] = 'No groupings';
+$string['numposts'] = 'How many posts to display';
---
+++ lib/editor/atto/plugins/insertforum/lang/he/atto_insertforum.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_insertforum', language 'en'.
+ *
+ * @package    atto_insertforum
+ * @author     Nadav Kavalerchik <nadavkav@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['forum'] = 'להוסיף פורום';
+$string['insertforum'] = 'הוספת פורום (מסנן)';
+$string['noforums'] = 'לא נמצא פורום שניתן להוסיף';
+$string['pluginname'] = 'הוספת פורום';
+$string['select_desc'] = 'נא לבחור הפורום שברצונך להוסיף.  שימו לב, משתמשים לא יוכלו לראות את הפורום אם אין להם הרשאות מתאימות';
+//$string['select_desc'] = 'Select the Forum you want to insert below. Note, users may not be able to see the forum if any of its visiblity options and/or the users permissions prevent them from seeing it.';
+$string['title'] = 'בחירת פורום';
+$string['selectgroup'] = 'בחירת קבוצה';
+$string['groups'] = 'קבוצות';
+$string['nogroups'] = 'אין קבוצות';
+$string['selectgrouping'] = 'בחירת אוסף קבוצות';
+$string['groupings'] = 'אוספי קבוצות';
+$string['nogroupings'] = 'אין אוספי קבוצות';
+$string['numposts'] = 'כמה תגובות להציג';
