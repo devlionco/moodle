229086f01b39cf20086babba52d4b9f960963754
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-04-15 22:00:19 +0300

    qtype/calcsheet (fix) PHP Fatal error:  Declaration of qtype_calcsheet_test::setUp() must be compatible with PHPUnit\Framework\TestCase::setUp(): void

--- question/type/calcsheet/tests/questiontype_test.php
+++ question/type/calcsheet/tests/questiontype_test.php
@@ -45,7 +45,7 @@ class qtype_calcsheet_test extends advanced_testcase {
 
     protected $qtype;
 
-    protected function setUp() {
+    protected function setUp() : void {
         $this->qtype = new qtype_calcsheet();
     }
 
