861d87c2ff58c951fd8dbcfe35a59296ed7c49bb
oleg <oleg@devlion.co>
2023-04-23 20:29:16 +0300

    PTL #8622 block_oer_items (add) Changes from m39_petel (#8556, #787, #8337, #5253, #5118)

---
+++ blocks/oer_items/classes/external.php
@@ -0,0 +1,103 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External course API
+ *
+ * @package    block_oer_items_external
+ * @category   external
+ * @copyright  2019 Devlion <info@devlion.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->libdir . '/externallib.php');
+require_once($CFG->dirroot . '/blocks/oer_items/lib.php');
+
+/**
+ * Course external functions
+ *
+ * @package    core_course
+ * @category   external
+ * @copyright  2011 Jerome Mouneyrac
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @since Moodle 2.2
+ */
+class block_oer_items_external extends external_api {
+
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     * @since Moodle 2.2
+     */
+    public static function render_activity_block_parameters() {
+        return new external_function_parameters(
+                array(
+                        'courseids' => new external_value(PARAM_TEXT, 'Course ids'),
+                        'language' => new external_value(PARAM_RAW, 'Language'),
+                )
+        );
+    }
+
+    /**
+     * Send private messages from the current USER to other users
+     *
+     * @param array $messages An array of message to send.
+     * @return string
+     * @since Moodle 2.2
+     */
+    public static function render_activity_block($courseids, $language) {
+        global $OUTPUT, $DB, $USER;
+
+        $context = context_user::instance($USER->id);
+        self::validate_context($context);
+
+        $courses = array();
+        if (!empty($courseids)) {
+            $courses = json_decode($courseids);
+        }
+
+        $DB->delete_records('block_oer_items', array('userid' => $USER->id));
+
+        foreach ($courses as $courseid) {
+            $DB->insert_record('block_oer_items', array(
+                    'userid' => $USER->id,
+                    'courseid' => $courseid,
+                    'timemodified' => time()
+            ));
+        }
+
+        $data = block_oer_items_render_activities_content($language, $courses);
+
+        return json_encode([
+                'data' => $data,
+                'pix_no_courses' => $OUTPUT->image_url('courses', 'block_oer_items')->out()
+        ]);
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_description
+     * @since Moodle 2.2
+     */
+    public static function render_activity_block_returns() {
+        return new external_value(PARAM_RAW, 'The blocks settings');
+    }
+
+}
---
+++ blocks/oer_items/lang/en/block_oer_items.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_oer_items', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package     block_oer_items
+ * @copyright   2019 Devlion <info@devlion.co>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Oer items';
+$string['noactivities'] = 'No activities';
+$string['settingstag'] = 'Tag';
+$string['settingstagdesc'] = 'Enter tag';
+$string['removecourse'] = 'Remove course from block';
+$string['selectcourse'] = 'Select course';
+$string['cacheenable'] = 'Enable cache';
+$string['cacheenabledesc'] = 'Enable/Disable cache';
+$string['cachedef_course'] = 'OER items (block) cache course activities';
+
+$string['privacy:metadata'] = 'The Courses block only shows data about courses and does not store any data itself.';
+$string['oer_items:addinstance'] = 'Add instance';
+$string['oer_items:myaddinstance'] = 'Add instance to MY page';
+
+$string['selectallcourses'] = 'All courses';
+$string['selectalllanguages'] = 'All languages';
