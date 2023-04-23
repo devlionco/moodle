e4c1eb6cbd0439995c519a10ff8908f002607803
oleg <oleg@devlion.co>
2023-04-23 21:21:21 +0300

    PTL #8622 block_social_newcourses (add) Changes from m39_petel (#3870, #3009, #3012, #787)

---
+++ blocks/social_newcourses/lang/he/block_social_newcourses.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_social_newcourses', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_social_newcourses
+ * @copyright  2019 Devlion <info@devlion.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'חדש מסביבת העמיתים';
+$string['nocourses'] = 'אין קורסים';
+$string['selectperpage'] = 'פריטים בדף';
+$string['teachersoncourse'] = 'מורים עמיתים בקורס הזה:';
+$string['onpage3'] = '3 פריטים';
+$string['onpage5'] = '5 פריטים';
+$string['onpage10'] = '10 פריטים';
+$string['onpage15'] = '15 פריטים';
+$string['onpage25'] = '25 פריטים';
+$string['onpage50'] = '50 פריטים';
+$string['privacy:metadata'] = 'חסימת הקורסים מציגה רק נתונים על קורסים ואינה אוגרת נתונים כלשהם עצמה.';
+$string['social_newcourses:addinstance'] = 'הוסף מופע';
+$string['social_newcourses:myaddinstance'] = 'הוסף מופע לדף שלי';
---
+++ blocks/social_newcourses/lib.php
@@ -0,0 +1,103 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course list block.
+ *
+ * @package    block_social_newcourses
+ * @copyright  2019 Devlion <info@devlion.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/course/lib.php');
+require_once($CFG->dirroot . '/local/community/plugins/social/locallib.php');
+
+function block_social_newcourses_render_content_block() {
+    global $OUTPUT;
+
+    // Build select.
+    $perpage = array(
+            array('name' => get_string('onpage3', 'block_social_newcourses'), 'active' => true, 'value' => '3'),
+            array('name' => get_string('onpage5', 'block_social_newcourses'), 'active' => false, 'value' => '5'),
+            array('name' => get_string('onpage10', 'block_social_newcourses'), 'active' => false, 'value' => '10'),
+            array('name' => get_string('onpage15', 'block_social_newcourses'), 'active' => false, 'value' => '15'),
+            array('name' => get_string('onpage25', 'block_social_newcourses'), 'active' => false, 'value' => '25'),
+            array('name' => get_string('onpage50', 'block_social_newcourses'), 'active' => false, 'value' => '50'),
+    );
+
+    $pagevalue = 3;
+    $render = '';
+
+    $data = new \StdClass();
+    $data->perpage = $perpage;
+    $data->has_select_menu = !empty($perpage) ? true : false;
+    $data->content = $render;
+    $data->content_empty = !empty($render) ? false : true;
+    $data->page_value = $pagevalue;
+    $data->pix_no_courses = $OUTPUT->image_url('courses', 'block_social_newcourses');
+
+    return $OUTPUT->render_from_template('block_social_newcourses/layout', $data);
+}
+
+function block_social_newcourses_render_courses_content($perpage = 1) {
+    global $OUTPUT, $USER, $DB;
+
+    $html = '';
+
+    $coursespombim = array();
+    $followers = social_teachers_get_followers();
+    foreach ($followers as $follower) {
+
+        $data = social_get_courses_pombim($follower->id);
+        foreach ($data['courses_pombim'] as $course) {
+
+            // Exclude courses.
+            $shared = $DB->get_record('community_social_shrd_crss', array('userid' => $follower->id, 'courseid' => $course->id));
+
+            if ($course->button_send_request) {
+                $course->userid = $follower->id;
+                $course->username = $follower->fullname;
+                $course->userimageurl = $follower->image_url;
+                $course->sharedtime = $shared->timecreated;
+                $course->countcollegues = count($course->collegues);
+                $course->currentuserid = $USER->id;
+
+                if (strlen($course->summary) > 200) {
+                    $course->summary = mb_substr(strip_tags($course->summary), 0, 200) . '...';
+                }
+
+                $coursespombim[] = $course;
+            }
+        }
+    }
+
+    usort($coursespombim, "block_social_newcourses_cmp");
+
+    // Perpage.
+    for ($i = 0; $i < $perpage; $i++) {
+        if (isset($coursespombim[$i])) {
+            $html .= $OUTPUT->render_from_template('block_social_newcourses/course-item', $coursespombim[$i]);
+        }
+    }
+
+    return $html;
+}
+
+function block_social_newcourses_cmp($a, $b) {
+    return $a->sharedtime < $b->sharedtime;
+}
