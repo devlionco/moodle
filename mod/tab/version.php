816360bea532e5a9b7fa30470c53e37c3579a1a6
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-04-23 12:44:00 +0300

    mod/tab (new) ver.2023011900 m41

---
+++ mod/tab/version.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * *************************************************************************
+ * *                         OOHOO - Tab Display                          **
+ * *************************************************************************
+ * @package     mod                                                       **
+ * @subpackage  tab                                                       **
+ * @name        tab                                                       **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau                                        **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+$plugin->version = 2023011900;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2020061500.00; // Moodle 3.9.0
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v1.16.1-stable';
+$plugin->component = 'mod_tab';
+$plugin->supported = [39, 410];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
+
