6a771af25df12a3f2cd3b585844f5694f6577ef9
oleg <oleg@devlion.co>
2023-04-24 13:15:55 +0300

    PTL #8622 block_oer_reviews (add) Changes from m39_petel (#4006, #2933, #646, #4658, #8051)

---
+++ blocks/oer_reviews/block_oer_reviews.php
@@ -0,0 +1,267 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * oer_reviews block.
+ *
+ * @package    block_oer_reviews
+ * @copyright  2020 Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_oer_reviews extends block_base {
+
+    /**
+     * Initializes class member variables.
+     */
+    public function init() {
+        // Needed by Moodle to differentiate between blocks.
+        $this->title = get_string('pluginname', 'block_oer_reviews');
+    }
+
+    /**
+     * Returns the block contents.
+     *
+     * @return stdClass The block contents.
+     */
+    public function get_content() {
+        global $DB, $OUTPUT, $USER;
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        if (!$this->is_user_real_teacher()) {
+            return $this->content;
+        }
+
+        if (empty($this->instance)) {
+            $this->content = '';
+            return $this->content;
+        }
+
+        $this->content = new stdClass();
+
+        if (!empty($this->config->text)) {
+            $this->content->text = $this->config->text;
+        } else {
+            $this->title = get_string('title', 'block_oer_reviews');
+
+            $text = html_writer::div(get_string('intro', 'block_oer_reviews'), 'intro', ['id' => 'fade-it']);
+
+            // Scrolling.
+            $text .= '<div class="overflow-auto p-3" style="max-height: 25rem;">';
+
+            $maxusers = trim(get_config('block_oer_reviews', 'items'));
+            if (empty($maxusers) || !is_numeric($maxusers)) {
+                $maxusers = 25;
+            }
+
+            $latestreviewers = [];
+
+            // Get all ("good") reviews with Hebrew YES Default.
+            if (!$this->is_user_orderbydate() && !$this->is_user_orderbyrelationship()) {
+                $sql = "SELECT * FROM {community_oercatalog_reviews}
+                    WHERE json_value(feedback, '$.question1') LIKE '%"
+                        . get_string('yes') . "%' AND reviewtype = 'activity'
+                    ORDER BY timecreated DESC";
+                $latestreviewers = $DB->get_records_sql($sql);
+            }
+
+            // Get all ("good") reviews with Hebrew YES order by date.
+            if ($this->is_user_orderbydate()) {
+                $sql = "SELECT * FROM {community_oercatalog_reviews}
+                    WHERE json_value(feedback, '$.question1') LIKE '%"
+                        . get_string('yes') . "%' AND reviewtype = 'activity'
+                    ORDER BY timecreated DESC";
+                $latestreviewers = $DB->get_records_sql($sql);
+            }
+
+            // Get all ("good") reviews with Hebrew YES order by relationship.
+            if ($this->is_user_orderbyrelationship()) {
+                $usersfeedback = [];
+                foreach ($DB->get_records('social_relationships', ['userid_watching' => $USER->id]) as $item) {
+                    $usersfeedback[] = $item->userid_feedback;
+                }
+
+                if (!empty($usersfeedback)) {
+                    $sql = "
+                        SELECT sor.*, srs.points
+                        FROM {community_oercatalog_reviews} sor
+                        LEFT JOIN {social_relationships} srs
+                            ON (srs.userid_watching = " . $USER->id . " AND srs.userid_feedback = sor.userid)
+                        WHERE json_value(sor.feedback, '$.question1') LIKE '%" . get_string('yes') . "%'
+                            AND sor.reviewtype = 'activity'
+                        ORDER BY srs.points DESC";
+                    $latestreviewers = $DB->get_records_sql($sql);
+                }
+            }
+
+            $counter = 0;
+            foreach ($latestreviewers as $reviewer) {
+                $user = $DB->get_record('user', ['id' => $reviewer->userid]);
+                $userpicture = $OUTPUT->user_picture($user);
+                $username = fullname($user);
+                $feedback = json_decode($reviewer->feedback);
+                $feedbacktooltip = '';
+
+                if (!isset($feedback->descr)) {
+                    continue;
+                }
+
+                $feedbacktooltip .= $feedback->descr;
+
+                $mid = \local_metadata\mcontext::module()->get($reviewer->objid, 'ID');
+                if (empty($mid)) {
+                    $mid = $reviewer->objid;
+                }
+
+                $activityurl = new \moodle_url('/local/community/plugins/oer/activityshare.php?id=',
+                        ['id' => $mid, 'source' => 'oerreviewsblock']);
+
+                $sql = "
+                    SELECT
+                        CASE m.name
+                            WHEN 'assign' THEN (SELECT asg.name FROM {assign} asg WHERE asg.id = cm.instance)
+                            WHEN 'forum' THEN (SELECT f.name FROM {forum} f WHERE f.id = cm.instance)
+                            WHEN 'quiz' THEN (SELECT q.name FROM {quiz} q WHERE q.id = cm.instance)
+                            WHEN 'questionnaire' THEN (SELECT q.name FROM {questionnaire} q WHERE q.id = cm.instance)
+                            WHEN 'hvp' THEN (SELECT q.name FROM {hvp} q WHERE q.id = cm.instance)
+                            WHEN 'resource' THEN (SELECT q.name FROM {resource} q WHERE q.id = cm.instance)
+                            WHEN 'page' THEN (SELECT q.name FROM {page} q WHERE q.id = cm.instance)
+                            WHEN 'folder' THEN (SELECT q.name FROM {folder} q WHERE q.id = cm.instance)
+                            WHEN 'ejsapp' THEN (SELECT q.name FROM {ejsapp} q WHERE q.id = cm.instance)
+                            WHEN 'url' THEN (SELECT q.name FROM {url} q WHERE q.id = cm.instance)
+                            WHEN 'workshop' THEN (SELECT q.name FROM {workshop} q WHERE q.id = cm.instance)
+                            WHEN 'data' THEN (SELECT q.name FROM {data} q WHERE q.id = cm.instance)
+                            WHEN 'glossary' THEN (SELECT q.name FROM {glossary} q WHERE q.id = cm.instance)
+                            WHEN 'game' THEN (SELECT q.name FROM {game} q WHERE q.id = cm.instance)
+                            WHEN 'checklist' THEN (SELECT q.name FROM {checklist} q WHERE q.id = cm.instance)
+                        END AS 'oername'
+                    FROM {course_modules} cm
+                     JOIN {modules} m ON m.id = cm.module
+                    WHERE cm.id = ?";
+                $activityinfo = $DB->get_record_sql($sql, [$reviewer->objid]);
+
+                $activity = html_writer::link($activityurl, $activityinfo->oername,
+                        ['aria-label' => $activityinfo->oername . ' ' . strip_tags($feedbacktooltip)]);
+
+                $text .= html_writer::tag('span',
+                        "<div class='row'>
+                        <div class='col-3'>$userpicture</div>
+                        <div class='col-9'>$username</div>
+                        <div class='col-3'></div>
+                        <div class='col-9' data-toggle='tooltip' data-html='true' title='$feedbacktooltip'>
+                            <i class='fa fa-info-circle'></i> $activity</div>
+                     </div><br>", ['class' => 'reviewer']);
+
+                $counter++;
+
+                if ($counter >= $maxusers) {
+                    break;
+                }
+            }
+
+            $text .= '</div>';
+
+            $this->content->text = $text;
+            $this->content->footer = '';
+        }
+
+        return $this->content;
+    }
+
+    /**
+     * Defines configuration data.
+     *
+     * The function is called immediatly after init().
+     */
+    public function specialization() {
+
+        // Load user defined title and make sure it's never empty.
+        if (empty($this->config->title)) {
+            $this->title = get_string('pluginname', 'block_oer_reviews');
+        } else {
+            $this->title = $this->config->title;
+        }
+    }
+
+    /**
+     * Enables global configuration of the block in settings.php.
+     *
+     * @return bool True if the global configuration is enabled.
+     */
+    public function has_config() {
+        return true;
+    }
+
+    /**
+     * Sets the applicable formats for the block.
+     *
+     * @return string[] Array of pages and permissions.
+     */
+    public function applicable_formats() {
+        return array('all' => false, 'my' => true);
+    }
+
+    /**
+     * @param null $userid - userid of real teacher. otherwise use $USER.
+     * @return false|mixed
+     * @throws dml_exception
+     */
+    public function is_user_real_teacher() {
+        global $CFG;
+
+        if (!isset($CFG->reviewcohort) || empty($CFG->reviewcohort)) {
+            return false;
+        }
+
+        $teacherscohort = $CFG->reviewcohort;
+        return $this->is_teacher_in_cohort($teacherscohort);
+    }
+
+    public function is_user_orderbydate() {
+        global $CFG;
+
+        if (!isset($CFG->eladresearch_cohort_orderbydate) || empty($CFG->eladresearch_cohort_orderbydate)) {
+            return false;
+        }
+
+        $teacherscohort = $CFG->eladresearch_cohort_orderbydate;
+        return $this->is_teacher_in_cohort($teacherscohort);
+    }
+
+    public function is_user_orderbyrelationship() {
+        global $CFG;
+
+        if (!isset($CFG->eladresearch_cohort_orderbyrelationship) || empty($CFG->eladresearch_cohort_orderbyrelationship)) {
+            return false;
+        }
+
+        $teacherscohort = $CFG->eladresearch_cohort_orderbyrelationship;
+        return $this->is_teacher_in_cohort($teacherscohort);
+    }
+
+    private function is_teacher_in_cohort($cohortid) {
+        global $DB, $USER;
+
+        $sql = "SELECT *
+                    FROM {cohort_members} cm
+                    JOIN {cohort} c ON c.id = cm.cohortid
+                    WHERE c.idnumber=? AND cm.userid=?";
+
+        return $DB->get_records_sql($sql, [$cohortid, $USER->id]);
+    }
+}
