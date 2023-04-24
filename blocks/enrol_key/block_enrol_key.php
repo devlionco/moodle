4e33b3c19e20414aedf71af4d2f50a2c0d533c09
oleg <oleg@devlion.co>
2023-04-24 10:40:50 +0300

    PTL #8622 block_enrol_key (add) Changes from m39_petel (#6445)

---
+++ blocks/enrol_key/block_enrol_key.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_enrol_key
+ * @copyright  Matan berkovitch <matan.berkovitch@weizmann.ac.il>
+ */
+class block_enrol_key extends block_base {
+
+    /**
+     * block initializations
+     */
+    public function init() {
+        $this->title = get_string('pluginname', 'block_enrol_key');
+    }
+
+    public function get_content() {
+        global $CFG;
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $text = '
+            <div id="enrolkeyform" class="d-inline-block ">
+                <form action="' . $CFG->wwwroot . '/enrol/self/enrolwithkey.php" method="post">
+                    <input type="text" id="enrolkey" name="enrolkey" size="10" maxlength="15"
+                            placeholder="' . get_string('enrol_key', 'block_enrol_key') . '">
+                    <input class="btn btn-primary btn-sm px-20 font-weight-500 font-size-14 h-35" type="submit" 
+                            value="' . get_string('enrol_me', 'block_enrol_key') . '" style="margin-top: -6px;">
+                </form>
+            </div>';
+
+        $this->content = new stdClass;
+        $this->content->text = $text;
+        return $this->content;
+
+    }
+
+    public function applicable_formats() {
+        return array('all' => false, 'mod' => false, 'my' => true);
+    }
+}
---
+++ blocks/enrol_key/lang/en/block_enrol_key.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_enrol_key', language 'en', branch
+ *
+ * @package    block_enrol_key
+ * @copyright  matan berkovitch <matan.berkovitch@weizmann.ac.il>
+ */
+
+$string['pluginname'] = 'Registration for an additional course';
+$string['enrol_key'] = 'Enrol key';
+$string['enrol_me'] = 'Enrol me';
+$string['enrol_key:addinstance'] = 'Add instance';
+$string['enrol_key:myaddinstance'] = 'Add instance to MY page';
