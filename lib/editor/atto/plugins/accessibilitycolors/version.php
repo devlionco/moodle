2d446cde7003c5542258f65b56c92a4760fa0263
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-05-11 00:41:18 +0300

    atto/accessibilitycolors (new) ver.2022041400 M3.11

---
+++ lib/editor/atto/plugins/accessibilitycolors/version.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Atto text editor accessibility colors plugin, version file.
+ *
+ * @package    atto_accessibilitycolors
+ * @copyright  2022 Weizmann institute of science, Israel.
+ * @author     Oleg Ochkurenko [Devlion] <oleg@devlion.co>
+ * @author     Nadav Kavalerchik [Weizmann, Science teaching department] <nadav.kavalerchik@weizmann.ac.il>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2022041400;
+$plugin->requires  = 2014050800;
+$plugin->component = 'atto_accessibilitycolors';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0 for Moodle 2.7-3.11 (build 2022041400)';
+
