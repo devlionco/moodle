cccbd078774d1af54b76f994025c9c92faca9b67
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-05-14 15:49:01 +0300

    local/notifycohorts (new) ver.2021062200 m310

---
+++ local/notifycohorts/README.md
@@ -0,0 +1,22 @@
+# Notify Cohorts #
+
+This plugin allows to send notifications to the members of selected cohorts.
+You just need to go to "Site adminsitration / Users / Notify cohorts", type the subsject and the body you want for the notification and select which cohorts will be notified.
+The notification will be sent as "system notification" on Moodle, not as a message from the user.
+Only users with 'moodle/site:config' capability permitted can send notifications.
+
+## License ##
+
+2021 Daniel Neis Araujo <daniel@adapta.online>
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ local/notifycohorts/version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     local_notifycohorts
+ * @copyright   2021 Daniel Neis Araujo <daniel@adapta.online>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_notifycohorts';
+$plugin->release = '3';
+$plugin->version = 2021062200;
+$plugin->requires = 2019111800; // Requires 3.8 version.
+$plugin->maturity = MATURITY_STABLE;
