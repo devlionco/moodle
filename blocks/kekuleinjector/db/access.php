9363922ae87e2524c97d95fd965e4eff194e1040
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-05-14 15:46:33 +0300

    block/kekuleinjector (new) ver.2016092201 copy from PETEL 3.9

---
+++ blocks/kekuleinjector/db/access.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * kekuleinjector block caps.
+ *
+ * @package    block_kekuleinjector
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    'block/kekuleinjector:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/kekuleinjector:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
