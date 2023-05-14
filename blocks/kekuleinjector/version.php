1731b389931671cd0b1f330204bcc25710f60bb8
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-05-14 15:46:33 +0300

    block/kekuleinjector (new) ver.2016092201 copy from PETEL 3.9

---
+++ blocks/kekuleinjector/version.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Version details
+ *
+ * @package    block_kekuleinjector
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_kekuleinjector';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
+$plugin->version = 2016092201;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
