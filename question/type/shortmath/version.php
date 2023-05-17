dfbd0fbf2c5da1aae9ee47c684700203dcd356a2
oleg <oleg@devlion.co>
2023-05-17 10:36:38 +0300

    PTL #8623 qtype_shortmath (fix) Changes from m39_petel (#1680, #6329, #4693, #7623).

--- question/type/shortmath/version.php
+++ question/type/shortmath/version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021080400;
+$plugin->version  = 2021080401;
 $plugin->requires = 2018051700; // Moodle version 3.5.
 $plugin->component = 'qtype_shortmath';
 $plugin->maturity  = MATURITY_STABLE;
