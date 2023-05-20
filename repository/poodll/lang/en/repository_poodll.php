10579031bd2a4620c7adfd5723790578992d481b
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-05-21 00:01:33 +0300

    repository/poodll (new) ver.2019112100 m310

---
+++ repository/poodll/lang/en/repository_poodll.php
@@ -0,0 +1,22 @@
+<?php // $Id$
+$string['pluginname'] = 'Poodll';
+$string['repositoryname'] = 'Poodll';
+$string['configplugin'] = 'Configure Poodll Repository';
+$string['pluginname_help'] = 'Poodll repository';
+$string['poodll:view'] = 'View Poodll Plus repository';
+$string['privacy:metadata'] = 'The Poodll Repository plugin does not store any personal data.';
+$string['account'] = 'Account information';
+
+$string['recordnew'] = 'New recording';
+$string['unexpectedevent'] = 'An unexpected event was detected. Consider reporting this problem to the Moodle developers.';
+
+$string['save'] = 'Save';
+$string['name'] = 'Name';
+$string['audio'] = 'Audio Recorder';
+$string['video'] = 'Video Recorder';
+$string['snapshot'] = 'Snapshot(Webcam)';
+$string['whiteboard'] = 'Poodll Whiteboard';
+$string['recording_format'] = 'Poodll Repository Type';
+$string['hide_player_opts'] = 'Audio Player Selection';
+$string['hide_player_opts_show'] = 'Show the audio player selection screen after recording audio.';
+$string['hide_player_opts_hide'] = 'Hide the audio player selection screen after recording audio.';
