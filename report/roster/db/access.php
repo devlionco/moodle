b8b069290d6ceee6e70b52070787183b8aa6edc1
oleg <oleg@devlion.co>
2023-06-05 10:46:49 +0300

    PTL #8623 report_roster (fix) Changes from m39_petel. (#3322, #3120, #3846, #6411)

--- report/roster/db/access.php
+++ report/roster/db/access.php
@@ -34,5 +34,15 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         ),
         'clonepermissionsfrom' => 'moodle/site:viewreports',
-    )
+    ),
+    'report/roster:resetpassword' => array(
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'guest' => CAP_PROHIBIT,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
 );
