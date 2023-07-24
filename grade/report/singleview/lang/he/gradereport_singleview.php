92c9d49b0113d954a798c480c90167da7f047e0e
oleg <oleg@devlion.co>
2023-07-24 15:04:10 +0300

    PTL #9279, #9280, #9281 gradereport_singleview (update) Add Hebrew translate.

---
+++ grade/report/singleview/lang/he/gradereport_singleview.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for Single view
+ *
+ * @package   gradereport_singleview
+ * @copyright 2014 Moodle Pty Ltd (http://moodle.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// General Strings.
+$string['bulkchoice'] = 'הוספה בתפזורת מאפשרת לך להחליף ציונים מרובים בבת אחת. בחר את הציונים שברצונך להחליף:';
+$string['excludeallgrades'] = 'אל תכלול הכל';
+$string['excludenonegrades'] = 'אל תכלול אף אחד';
+$string['gotonextreport'] = 'עבור לדוח המשתמש הבא';
+$string['gotopreviousreport'] = 'עבור לדוח המשתמש הקודם';
+$string['overrideallgrades'] = 'תעקוף הכל';
+$string['overridenonegrades'] = 'תעקוף אף אחד';
+$string['overridenoneconfirm'] = 'פעולה זו תסיר את כל הציונים שהוזנו בעבר בדף זה כשתשמור שינויים.';
+$string['privacy:metadata'] = 'דוח תצוגה יחידה של ציון מציג רק נתונים המאוחסנים במיקומים אחרים.';
+$string['removeoverride'] = 'הסר עקיפות ציונים';
+$string['removeoverridesave'] = 'הסר עקיפות';
+$string['unsavedataalert'] = 'אם יש לך שינויים שלא נשמרו בדף, תאבד אותם אם תמשיך עם ציונים להוספת כמות גדולה.';
+$string['unsavedataconfirm'] = 'אני מבין שהשינויים שלא נשמרו יאבדו.';
+$string['ariareporttype'] = 'בחר סוג דוח להצגה';
+$string['viewsinglegradeitem'] = 'בחר פריט ציון למעלה';
+$string['searchgrades'] = 'חפש פריטי ציון';
+$string['selectagrade'] = 'בחר פריט ציון';
+$string['selectgradeitemlink'] = 'בחר פריט ציון';
+$string['viewsingleuserorgradeitem'] = 'הצג את כל הציונים של משתמש או פריט ציון בודד.';
+$string['viewby'] = 'הצג לפי';
+$string['viewsingleuser'] = 'בחר משתמש למעלה כדי לראות את כל הציונים שלו';
+$string['singleviewdescription'] = 'בתצוגה יחידה אתה יכול לעקוף או לא לכלול ציונים נבחרים ולהוסיף ציונים בכמות גדולה.';
+$string['unsaveddatawarning'] = 'אזהרת נתונים שלא נשמרו';
