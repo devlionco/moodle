0ebc249818c8fa5ef7fcfd1d3e5abb379afa812b
oleg <oleg@devlion.co>
2023-07-24 12:04:26 +0300

    PTL #9312 message_petel (update) Fix error.

--- message/output/petel/notifications.php
+++ message/output/petel/notifications.php
@@ -37,11 +37,11 @@ $PAGE->set_url($url);
 require_login();
 
 if (isguestuser()) {
-    throw new \moodle_exception(('guestnoeditmessage', 'message');
+    throw new \moodle_exception('guestnoeditmessage', 'message');
 }
 
 if (!$user = $DB->get_record('user', ['id' => $userid])) {
-    throw new \moodle_exception(('invaliduserid');
+    throw new \moodle_exception('invaliduserid');
 }
 
 $personalcontext = context_user::instance($user->id);
