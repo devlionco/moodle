3086ed3310307882c85347a1bb28ab9f821af7ea
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-08-07 00:00:37 +0300

    question/type/opensheet (lang) Add Hebrew translation

--- question/type/opensheet/lang/he/qtype_opensheet.php
+++ question/type/opensheet/lang/he/qtype_opensheet.php
@@ -23,8 +23,43 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'גליון נתונים + גרף (פתוחה)';
+
 $string['combinedcontrolnameopensheet'] = 'חסר תוכן בגליון נתונים + גרף (פתוחה)';
+$string['acceptedfiletypes'] = 'סוגי קבצים מצורפים אפשריים';
+$string['acceptedfiletypes_help'] = 'Accepted file types can be restricted by entering a list of file extensions. If the field is left empty, then all file types are allowed.';
+$string['allowattachments'] = 'אפשרות לקבצים מצורפים';
+$string['attachedfiles'] = 'קבצים מצורפים: {$a}';
+$string['attachmentsoptional'] = 'לא נדרשים קבצים מצורפים';
+$string['attachmentsrequired'] = 'נדרשים קבצים מצורפים';
+$string['attachmentsrequired_help'] = 'This option specifies the minimum number of attachments required for a response to be considered gradable.';
+$string['formateditor'] = 'תוכן מעוצב';
+$string['formateditorfilepicker'] = 'תוכן מעוצב וקבצים מצורפים';
+$string['formatmonospaced'] = 'תוכן לא מעוצב בגופן בעל ריווח אחיד';
+$string['formatnoinline'] = 'ללא הזנת תוכן';
+$string['formatplain'] = 'תוכן לא מעוצב';
+$string['graderinfo'] = 'מידע עבור בודקים';
+$string['graderinfoheader'] = 'מידע עבור בודקים';
+$string['mustattach'] = 'When "No online text" is selected, or responses are optional, you must allow at least one attachment.';
+$string['mustrequire'] = 'When "No online text" is selected, or responses are optional, you must require at least one attachment.';
+$string['mustrequirefewer'] = 'You cannot require more attachments than you allow.';
+$string['nlines'] = '{$a} שורות';
+$string['nonexistentfiletypes'] = 'סוגי הקבצים הללו לא מזוהים: {$a}';
+$string['pluginname'] = 'גליון נתונים + גרף (פתוחה)';
+$string['pluginname_help'] = 'In response to a question, the respondent may upload one or more files and/or enter text online. A response template may be provided. Responses must be graded manually.';
+$string['pluginname_link'] = 'question/type/opensheet';
+$string['pluginnameadding'] = 'הוספת שאלת גליון נתונים';
+$string['pluginnameediting'] = 'עריכת שאלת גליון נתונים';
+$string['pluginnamesummary'] = 'Allows a response of a file upload and/or online text. This must then be graded manually.';
+$string['privacy:metadata'] = 'The Opensheet question type plugin does not store any personal data.';
+$string['responsefieldlines'] = 'רוחב משבצת הזנה';
+$string['responseformat'] = 'תסדיר תשובה';
+$string['responseoptions'] = 'אפשרויות תשובה';
+$string['responserequired'] = 'תוכן נדרש';
+$string['responsenotrequired'] = 'לא נדרש תוכן';
+$string['responseisrequired'] = 'התלמיד נדרש להזין תוכן';
+$string['responsetemplate'] = 'תבנית תשובה';
+$string['responsetemplateheader'] = 'תבנית תשובה';
+$string['responsetemplate_help'] = 'Any text entered here will be displayed in the response input box when a new attempt at the question starts.';
 
 $string['charttitle'] = 'כותרת הגרף';
 $string['charttype'] = 'סוג הגרף';
