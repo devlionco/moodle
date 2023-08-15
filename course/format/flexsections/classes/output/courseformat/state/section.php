aed5b091307242f50b24863d503d06a640f6deb3
oleg <oleg@devlion.co>
2023-08-15 18:08:00 +0300

    PTL #9728 format_flexsections (update) Fix side menu.

--- course/format/flexsections/classes/output/courseformat/state/section.php
+++ course/format/flexsections/classes/output/courseformat/state/section.php
@@ -55,17 +55,32 @@ class section extends \core_courseformat\output\local\state\section {
         $data->haschildren = !empty($data->children);
         $data->singlesection = (int)($this->section->collapsed && $this->section->section == $this->format->get_viewed_section());
 
-        // PTL-9520.
-        $count = 0;
-        $arropened = [];
-        foreach ($this->format->get_modinfo()->get_section_info_all() as $item) {
-            if ($count <= 1) {
-                $arropened[] = $item->section;
-            }
-            $count++;
-        }
 
-        $data->indexcollapsed = in_array($this->section->section, $arropened) ? false : true;
+        $format = $this->format;
+        $preferences = $format->get_sections_preferences();
+
+        // PTL-9728.
+        if (!empty($preferences)) {
+            $data->indexcollapsed = true;
+            if (isset($preferences[$this->section->id])) {
+                $sectionpreferences = $preferences[$this->section->id];
+
+                if (!isset($sectionpreferences->indexcollapsed)) {
+                    $data->indexcollapsed = false;
+                }
+            }
+        }else{
+            $count = 0;
+            $arropened = [];
+            foreach ($this->format->get_modinfo()->get_section_info_all() as $item) {
+                if ($count == 0) {
+                    $arropened[] = $item->section;
+                }
+                $count++;
+            }
+
+            $data->indexcollapsed = in_array($this->section->section, $arropened) ? false : true;
+        }
 
         return $data;
     }
