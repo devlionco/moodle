d998df26dd9ac2887ef433a3b1e48c46d1270423
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2023-08-23 21:46:08 +0300

    PTL #9806 course/format/flexsection (perf) WIP Disabled and research for improved performance

--- course/format/flexsections/classes/output/courseformat/content/section.php
+++ course/format/flexsections/classes/output/courseformat/content/section.php
@@ -142,8 +142,10 @@ class section extends \core_courseformat\output\local\content\section {
         }
 
         // Lastseen
-        $lastseen = format_flexsections_lastseen($this->format->get_course()->id, $this->section->section, $USER->id);
-        $data->lastseen = $lastseen;
+        // PTL-9806 Disabled by nadav (TODO: Improve performance)
+        //$lastseen = format_flexsections_lastseen($this->format->get_course()->id, $this->section->section, $USER->id);
+        //$data->lastseen = $lastseen;
+        $data->lastseen = false;
 
         // Has share button.
         $data->hassharebutton = $PAGE->user_is_editing() ? true : false;
