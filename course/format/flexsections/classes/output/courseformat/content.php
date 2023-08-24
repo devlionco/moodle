18b140eb2947af92f28fba13115638bdd38c7416
oleg <oleg@devlion.co>
2023-08-24 11:14:54 +0300

    PTL #9706 quiz_advancedoverview (update) Fix section design.

--- course/format/flexsections/classes/output/courseformat/content.php
+++ course/format/flexsections/classes/output/courseformat/content.php
@@ -216,6 +216,8 @@ class content extends \core_courseformat\output\local\content {
 
         $data->allowedediting = has_capability('moodle/course:viewhiddensections', $coursecontext);
 
+        $data->hasblockavailabilityandcompletion = $data->hasavailability || $data->completion;
+
         return $data;
     }
 
