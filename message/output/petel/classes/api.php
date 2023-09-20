e3d6acb5c7c43b72fd0de63e3085d8a100767349
oleg <oleg@devlion.co>
2023-09-20 11:10:58 +0300

    PTL #10034 message_petel (update) Fix "emailstop" in message counter.

--- message/output/petel/classes/api.php
+++ message/output/petel/classes/api.php
@@ -102,6 +102,18 @@ class api {
             $useridto = $USER->id;
         }
 
+        // Is notification enabled.
+        if ($useridto == $USER->id) {
+            $disabled = $USER->emailstop;
+        } else {
+            $user = \core_user::get_user($useridto, "emailstop", MUST_EXIST);
+            $disabled = $user->emailstop;
+        }
+        if ($disabled) {
+            // Notifications are disabled.
+            return 0;
+        }
+
         return $DB->count_records_sql(
                 "SELECT count(id)
                FROM {notifications}
