083fa7b841ff7676d3ba7bc6fecd3427cc690bc5
oleg <oleg@devlion.co>
2023-11-14 16:24:53 +0200

    EC-317 mod_stickynotes (update) Remove copy notes.

--- mod/stickynotes/backup/moodle2/backup_stickynotes_stepslib.php
+++ mod/stickynotes/backup/moodle2/backup_stickynotes_stepslib.php
@@ -115,20 +115,22 @@ class backup_stickynotes_activity_structure_step extends backup_activity_structu
             FROM {stickynotes_column}
             WHERE stickyid = ?',
         array(backup::VAR_PARENTID));
-        $stickynotesnote->set_source_sql('
-            SELECT *
-            FROM {stickynotes_note}
-            WHERE stickyid = ?',
-        array(backup::VAR_PARENTID));
+
+        //$stickynotesnote->set_source_sql('
+        //    SELECT *
+        //    FROM {stickynotes_note}
+        //    WHERE stickyid = ?',
+        //array(backup::VAR_PARENTID));
+
         // All the rest of elements only happen if we are including user info.
         $stickynotescolumn->set_source_table('stickynotes_column', array('stickyid' => backup::VAR_ACTIVITYID));
-        $stickynotesnote->set_source_table('stickynotes_note', array('stickycolid' => backup::VAR_PARENTID));
+        //$stickynotesnote->set_source_table('stickynotes_note', array('stickycolid' => backup::VAR_PARENTID));
         $stickynotesvote->set_source_table('stickynotes_vote', array('stickynoteid' => backup::VAR_PARENTID));
 
 
         // Define id annotations.
         $stickynotesnote->annotate_ids('user', 'userid');
-        $stickynotesvote->annotate_ids('user', 'userid');
+        //$stickynotesvote->annotate_ids('user', 'userid');
 
         // Define file annotations.
         $stickynotes->annotate_files('mod_stickynotes', 'intro', null); // This file areas haven't itemid.
