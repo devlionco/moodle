d572918d6d22200eb7e3fc806227881b170026e2
Kiril Goldenshteyn <kiril@devlion.co>
2024-01-30 18:23:59 +0200

    EC-588 local/diagnostic (update) add running the proccess

---
+++ local/diagnostic/webrun.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Run the diagnostic for selected activities.
+ *
+ * @package    local_diagnostic
+ * @copyright  2021 Devlion
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../config.php');
+global $CFG, $DB;
+require_once $CFG->dirroot . '/local/diagnostic/classes/external.php';
+
+$cmid = required_param('cmid', PARAM_INT);
+
+if ($cache = \local_diagnostic\cache::get_record(['mid' => $cmid])) {
+    $cache->delete();
+}
+ob_start();
+@local_diagnotic_rebuild([$cmid]);
+$result = ob_get_clean();
